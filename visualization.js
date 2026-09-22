369921ab32dece6f2851be30baca2df64d256deb
BUNN1E5 <BUNN1E5@users.noreply.github.com>
2026-09-21 22:27:11 -0700

    Setting up Orillusion

--- visualization.js
+++ visualization.js
@@ -1,28 +1,10 @@
-// import * as Orillusion from 'orillusion';
+ import { Engine3D } from '@orillusion/core';
 
-// async function init() {
-//   const canvas = document.getElementById('canvas');
-//   const engine = new Orillusion.Engine3D({
-//     canvas,
-//   });
+async function init() {
+  let canvas = document.getElementById('canvas');
+  const engine = await Engine3D.init({
+    canvasConfig: { canvas }
+  });
+}
 
-//   await engine.init();
-
-//   // Create a simple scene
-//   const scene = new Orillusion.Scene3D();
-//   engine.sceneManager.addScene(scene);
-
-//   // Add camera
-//   const camera = new Orillusion.Camera3D();
-//   camera.transform.position.set(0, 5, 10);
-//   scene.addChild(camera);
-
-//   // Add a cube (or whatever 3D object you want)
-//   const cube = new Orillusion.Object3D();
-//   scene.addChild(cube);
-
-//   // Start render loop
-//   await engine.run();
-// }
-
-// init();
+init();
